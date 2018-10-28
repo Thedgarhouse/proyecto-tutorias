ab3936e86a327ebf132706e52a81a427d25dd91b
Edgar Mendoza <thedgarhouse@gmail.com>
2018-10-28 14:08:06 -0700

    Changed Sign in method and flow. Added Javadoc. P.D. Gradle Build is foobar.

--- app/src/main/java/com/example/edgar/asesoriasinformales/MainActivity.java
+++ app/src/main/java/com/example/edgar/asesoriasinformales/MainActivity.java
@@ -13,6 +13,12 @@ import android.view.View;
 import android.widget.Toast;
 
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.DexterError;
@@ -22,22 +28,32 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.PermissionRequestErrorListener;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final int RC_SIGN_IN = 123;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Choose authentication providers
+        // Establish under which options Google Sign In will be built
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Set Listener to Sign In button
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
     }
 
+    /**
+     * If user prompted to never show again the permission dialog for location, a dialog will be
+     * shown prompting the user to grant said permission through the Settings screen.
+     */
     private void showSettingsDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("Permiso requerido");
@@ -56,9 +72,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         builder.show();
-
     }
 
+    /**
+     * Opens intent to user's Settings screen.
+     */
     private void openSettings() {
         Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
         Uri uri = Uri.fromParts("package", getPackageName(), null);
@@ -66,7 +84,13 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 101);
     }
 
-    public void checkLocationPermission(View view) {
+    /**
+     * Nearby API requires Coarse location permission, which is tagged as a dangerous permission. In
+     * order to obtain it, the user must explicitly grant said permission at runtime. When the user
+     * clicks on button, a dialog prompt will ask for this permission from the user. Toast shows
+     * result to user. User is reminded that the app needs said permission if user doesn't grant it.
+     */
+    public void checkLocationPermission() {
         Dexter.withActivity(this).withPermission(Manifest.permission.ACCESS_COARSE_LOCATION).withListener(new PermissionListener() {
             @Override
             public void onPermissionGranted(PermissionGrantedResponse response) {
@@ -94,7 +118,89 @@ public class MainActivity extends AppCompatActivity {
         }).check();
     }
 
-    public void signIn(View v) {
+    /**
+     * Method that will create the intent for Google Sign In.
+     */
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
+    /**
+     * Verifies on activity start if a user has previously signed in. If previously signed in,
+     * account will hold the information, else it will be null.
+     * Update UI accordingly.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+//TODO: updateUI method according to View.
+    /**
+     * Update UI according to data received from Sign in method.
+     * @param account GoogleSignInAccount containing the users account if previously signed in.
+     *                If null, user hasn't signed in.
+     */
+    private void updateUI(GoogleSignInAccount account) {
+
+    }
+
+    /**
+     * Handler for different button clicks.
+     * @param v View holding the button clicked on main layout.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.check_permission:
+                checkLocationPermission();
+                break;
+        }
+
+    }
+
+    /**
+     * Handle results from calls for "startActivityForResult"
+     * @param requestCode The integer request code originally supplied to startActivityForResult(),
+     *                    allowing you to identify who this result came from.
+     * @param resultCode The integer result code returned by the child activity through its
+     *                   setResult().
+     * @param data An Intent, which can return result data to the caller (various data can be
+     *             attached to Intent "extras").
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    /**
+     * Handler for result of the Sign In operation.
+     * @param completedTask Task containing the result of the Sign in operation. Always completes.
+     */
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Sign in Handler", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
 }
