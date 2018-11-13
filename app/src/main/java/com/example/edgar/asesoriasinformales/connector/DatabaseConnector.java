49b4894fa9fb3f1babe4b66590e1ace7b8fc251a
Edgar Mendoza <thedgarhouse@gmail.com>
2018-11-13 01:16:46 -0800

    Added methods for testing.

--- app/src/main/java/com/example/edgar/asesoriasinformales/connector/DatabaseConnector.java
+++ app/src/main/java/com/example/edgar/asesoriasinformales/connector/DatabaseConnector.java
@@ -1,7 +1,14 @@
 package com.example.edgar.asesoriasinformales.connector;
 
+import android.content.Context;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -9,10 +16,16 @@ import com.google.firebase.database.FirebaseDatabase;
  * Created by Edgar on 11-Nov-18 with the Project Name: AsesoriasInformales.
  */
 public class DatabaseConnector {
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference myRef = database.getReference("message");
-    private DatabaseReference userReference = database.getReference("usuarios");
-    private DatabaseReference asesoryReference = database.getReference("asesorias");
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference myRef = firebaseDatabase.getReference("message");
+    private DatabaseReference userReference = firebaseDatabase.getReference("usuarios");
+    private DatabaseReference asesoryReference = firebaseDatabase.getReference("asesorias");
+    private String databaseURLusers = "https://tutorias-220600.firebaseio.com/usuarios.json";
+    private String simpleResponse;
+
+    public DatabaseConnector (){
+
+    }
 
     public void testHelloWorld(){
         myRef.setValue("Hello, World!");
@@ -20,7 +33,7 @@ public class DatabaseConnector {
     }
 
     public FirebaseDatabase getDatabase() {
-        return database;
+        return firebaseDatabase;
     }
 
     public DatabaseReference getMyRef() {
@@ -33,5 +46,13 @@ public class DatabaseConnector {
 
     public DatabaseReference getAsesoryReference() {
         return asesoryReference;
+    }
+
+    public void checkUser(String email, Context context){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String emailURLparameter = databaseURLusers +  "?orderBy=\"correo\"&equalTo=\"" + email + "\"&print=pretty";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, emailURLparameter,
+                response -> simpleResponse = response, error -> simpleResponse = error.toString());
+        queue.add(stringRequest);
     }
 }
