72e9078f58816f95386aa3c2e3ee8b0d60a7540a
David Ramirez <r.davidsaid06@gmail.com>
2018-11-14 07:36:00 -0800

    Caso de error al buscar asesoria

--- app/src/main/java/com/example/edgar/asesoriasinformales/Asesoria.java
+++ app/src/main/java/com/example/edgar/asesoriasinformales/Asesoria.java
@@ -10,16 +10,18 @@ public class Asesoria {
     public String alumno;
     public String asesor;
     public String inicio;
+    public String horas;
     public String fin;
 
     public Asesoria(){
 
     }
 
-    public Asesoria(String alumno, String asesor, String inicio, String fin) {
+    public Asesoria(String alumno, String asesor, String inicio, String fin,String horas) {
         this.alumno = alumno;
         this.asesor = asesor;
         this.inicio = inicio;
+        this.horas= horas;
         this.fin = fin;
     }
 }
