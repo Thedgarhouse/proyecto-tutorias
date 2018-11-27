dc2300b46d9c7353b67cda326189b9833cf8b65a
David Ramirez <r.davidsaid06@gmail.com>
2018-11-26 18:45:58 -0800

    Rama de vistas

--- app/src/main/java/com/example/edgar/asesoriasinformales/TeacherStats.java
+++ app/src/main/java/com/example/edgar/asesoriasinformales/TeacherStats.java
@@ -1,30 +1,36 @@
 package com.example.edgar.asesoriasinformales;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class TeacherStats extends AppCompatActivity implements View.OnClickListener {
-
-    private List<Asesoria> asesoriaList;
+public class TeacherStats extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_stats);
+        final Button miBoton= (Button) findViewById(R.id.botonHistorial);
+        miBoton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), HistorialActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
@@ -38,42 +44,9 @@ public class TeacherStats extends AppCompatActivity implements View.OnClickListe
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        asesoriaList = new ArrayList<>();
-                        try {
-                            JSONArray contacts = response.getJSONArray("asesorias");
-                            for (int i = 0; i < contacts.length(); i++) {
-                                if (contacts.get(i).toString().equals("null") == false) {
-                                    JSONObject c = contacts.getJSONObject(i);
-
-                                    String alumno = c.getString("alumno");
-                                    String asesor = c.getString("asesor");
-                                    String horas = c.getString("duracion");
-                                    String inicio = c.getString("inicio");
-                                    String fin = c.getString("fin");
-
-                                    asesoriaList.add(new Asesoria(alumno, asesor, inicio, fin, horas));
-                                }
-                            }
 
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
 
-                        TextView tiempoTotalAsesorias = (TextView)findViewById(R.id.tiempoTotal);
-                        TextView promedioAsesoria = (TextView)findViewById(R.id.promedioAsesoria);
-                        TextView cantidadAsesorias = (TextView)findViewById(R.id.cantidadAsesorias);
-
-                        int acumTiempo = 0;
-
-                        for(int x=0; x<asesoriaList.size(); x++)
-                        {
-                            acumTiempo = acumTiempo + Integer.parseInt(asesoriaList.get(x).getHoras().substring(0,0));
-                        }
-
-                        tiempoTotalAsesorias.setText(acumTiempo);
-                        promedioAsesoria.setText(acumTiempo/asesoriaList.size());
-                        cantidadAsesorias.setText(asesoriaList.size());
                     }
                 }, new Response.ErrorListener() {
 
@@ -99,6 +72,11 @@ public class TeacherStats extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
